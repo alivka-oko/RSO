2d739810a5984a5408eab79a4d4269247d2320b1
209-19 <alina.okotnikova2002@gmail.com>
2022-10-03 17:51:33 +0300

    v2

---
+++ 1Zadnanie.php
@@ -0,0 +1,35 @@
+<?php
+$connect=mysqli_connect('localhost', 'localhost', '123', 'onezadanie');
+$query = 'SELECT * FROM user;';
+$result = mysqli_query($connect, $query);
+
+?>
+
+<html>
+<head>
+</head>
+<body>
+<table>
+    <tr>
+        <th>Имя </th>
+        <th>Год </th>
+        <th>E-mail </th>
+    </tr>
+    <?php
+    while ($user=mysqli_fetch_array($result)){
+        echo"<tr> 
+            <td>{$user['username']}</td>
+            <td>{$user['year_birth']}</td>
+            <td>{$user['email']}</td>          
+             </tr>";} ?>
+</table>
+
+
+</body>
+
+
+
+
+
+
+</html>
